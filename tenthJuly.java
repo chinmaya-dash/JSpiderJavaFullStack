de30b8e86ea46b9ac65debac40276156159d4cc0
Chinmaya Dash <chinmayadash442004@gmail.com>
2025-07-13 11:53:07 +0530

    first commit

---
+++ tenthJuly.java
@@ -0,0 +1,24 @@
+public class tenthJuly {
+    public static void main(String[] args) {
+        int age = 21;
+        System.out.println("The name of the coder is : " + age);
+        Innernonpublic.greet();
+        INInnernonpublic.greet();
+    }
+
+}
+
+class Innernonpublic {
+    static void greet() {
+        // int age = 21;
+        System.out.println("inside Innernonpublic");
+
+    }
+}
+class INInnernonpublic {
+    static void greet() {
+        // int age = 21;
+        System.out.println("inside INInnernonpublic");
+
+    }
+}

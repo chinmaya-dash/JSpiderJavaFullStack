c8058491946b27bb5f6bce1c1e35b7917a6bd18f
Chinmaya Dash <chinmayadash442004@gmail.com>
2025-07-20 22:18:56 +0530

    biggersmallersmallest

---
+++ JSpiderJavaFullStack/scannerSc.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class scannerSc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Scanner used for Sum of two numbres entered from terminal
+        // System.out.print("Enter 2 numbers : ");
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // System.out.println("Sum of "+a+" & "+b+" is = "+(a+b));
+        // System.out.println("Sum of "+a+" & "+b+" is = "+(a+b));
+
+
+
+        sc.close();
+    }
+}

71d63a3fc73b50990f38c59e445eae82245b93ad
Chinmaya Dash <chinmayadash442004@gmail.com>
2025-08-09 17:17:13 +0530

    OptimizedPerClass

---
+++ FactoryMethode/recursionPractice.java
@@ -0,0 +1,18 @@
+package FactoryMethode;
+//package Recursion;
+//
+//public class recursionPractice {
+//    public static void main(String[] arg) {
+//        m1(1);
+//    }
+//
+//    static void m1(int i) {
+//        System.out.println(i);
+//        if (i >= 5) return;
+//        m1(++i);
+//        System.out.println(i);
+//        m1(++i);
+//        System.out.println(i);
+//    }
+//}
+//

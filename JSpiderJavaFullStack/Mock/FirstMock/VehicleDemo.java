3e17362fccc7805898115e8dc826d26c51d36049
Chinmaya Dash <chinmayadash442004@gmail.com>
2025-09-11 11:17:29 +0530

    complete

--- JSpiderJavaFullStack/Mock/FirstMock/VehicleDemo.java
+++ JSpiderJavaFullStack/Mock/FirstMock/VehicleDemo.java
@@ -1,4 +1,97 @@
 package Mock.FirstMock;
 
+// Abstraction + Encapsulation
+abstract class Vehicle {
+    private String brand;  // Encapsulation (private variable)
+
+    // Getter & Setter (Encapsulation)
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    // Abstract methods (Abstraction)
+    abstract void start();
+    abstract void stop();
+}
+
+// Inheritance + Polymorphism (Overriding)
+class Car extends Vehicle {
+    @Override
+    void start() {
+        System.out.println(getBrand() + " Car is starting with key ignition...");
+    }
+
+    @Override
+    void stop() {
+        System.out.println(getBrand() + " Car stopped.");
+    }
+}
+
+class Bike extends Vehicle {
+    @Override
+    void start() {
+        System.out.println(getBrand() + " Bike is starting with self start...");
+    }
+
+    @Override
+    void stop() {
+        System.out.println(getBrand() + " Bike stopped.");
+    }
+}
+
+// Interface (Complete Abstraction)
+interface MusicPlayer {
+    void playMusic();
+}
+
+// LuxuryCar extends Car + implements MusicPlayer
+class LuxuryCar extends Car implements MusicPlayer {
+    @Override
+    public void playMusic() {
+        System.out.println(getBrand() + " Luxury Car is playing music...");
+    }
+}
+
+// Compile-time Polymorphism (Method Overloading)
+class Driver {
+    void drive(Car c) {
+        System.out.println("Driving a Car...");
+    }
+
+    void drive(Bike b) {
+        System.out.println("Riding a Bike...");
+    }
+}
+// Main Class
 public class VehicleDemo {
+    public static void main(String[] args) {
+        // Upcasting (Parent reference -> Child object)
+        Vehicle v1 = new Car();
+        v1.setBrand("Toyota");
+        v1.start();   // Car start() called
+        v1.stop();
+
+        Vehicle v2 = new Bike();
+        v2.setBrand("Royal Enfield");
+        v2.start();   // Bike start() called
+        v2.stop();
+
+        // Using Interface with LuxuryCar
+        Vehicle v3 = new LuxuryCar();  // Upcasting
+        v3.setBrand("BMW");
+        v3.start();
+
+        // Downcasting to access MusicPlayer feature
+        LuxuryCar lc = (LuxuryCar) v3;
+        lc.playMusic();
+        lc.stop();
+
+        // Compile-time Polymorphism (Overloading)
+        Driver d = new Driver();
+        d.drive(new Car());
+        d.drive(new Bike());
+    }
 }
--- JSpiderJavaFullStack/Mock/FirstMock/abstractionPractice.java
+++ JSpiderJavaFullStack/Mock/FirstMock/abstractionPractice.java
@@ -1,4 +1,70 @@
 package Mock.FirstMock;
 
+interface bankATM{
+    void withdraw(double Amount);
+    void deposite(double Amount);
+    void balance();
+}
+class SBI implements bankATM{
+    private double balance = 2000;
+
+    @Override
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("SBI: Withdrawn " + amount);
+        } else {
+            System.out.println("SBI: Insufficient balance!");
+        }
+    }
+
+    @Override
+    public void deposite(double Amount) {
+            balance+=Amount;
+        System.out.println("deposited ammount is : "+Amount);
+    }
+
+    @Override
+    public void balance() {
+        System.out.println("Total balance is : "+balance);
+    }
+}
+class Axis implements bankATM{
+    private double balance = 5000;
+
+    @Override
+    public void withdraw(double Amount) {
+        if (Amount>balance){
+
+            System.out.println("Balance is insufficient");
+        }
+        else {
+            balance-=Amount;
+            System.out.println("Remaining balance : "+balance);
+        }
+    }
+
+    @Override
+    public void deposite(double Amount) {
+        balance+=Amount;
+        System.out.println("deposited ammount is : "+Amount);
+    }
+
+    @Override
+    public void balance() {
+        System.out.println("Total balance is : "+balance);
+    }
+}
 public class abstractionPractice {
+public static void main(String[] arg){
+    bankATM b = new SBI();
+    b.withdraw(1200);
+    b.deposite(1389);
+    b.balance();
+    bankATM a = new Axis();
+    a.withdraw(5000);
+    a.deposite(1209867345);
+    a.balance();
+}
+
 }

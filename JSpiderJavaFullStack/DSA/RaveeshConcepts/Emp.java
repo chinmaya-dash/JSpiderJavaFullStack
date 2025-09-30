918ea81f8ed76129d40772c8a1d3e4535e553358
Chinmaya Dash <chinmayadash442004@gmail.com>
2025-09-30 22:45:52 +0530

    empluyeeArraysort

---
+++ JSpiderJavaFullStack/DSA/RaveeshConcepts/Emp.java
@@ -0,0 +1,25 @@
+package DSA.RaveeshConcepts;
+
+public class Emp implements Comparable{
+    String name;
+    int id;
+    Float salary;
+    static int x = 100;
+    public Emp(String name, float salary){
+        this.salary = salary;
+        this.name = name;
+        this.id = x++;
+    }
+
+      @Override
+    public int compareTo(Object arg0) {
+        Emp e = ((Emp)arg0);
+        return name.compareTo(e.name);
+    }
+
+    @Override
+    public String toString(){
+        return "Employee[name = "+name+", Id = "+id+", Salary = "+salary+"]";
+    }
+    
+}
---
+++ JSpiderJavaFullStack/DSA/RaveeshConcepts/lengthEmpName.java
@@ -0,0 +1,16 @@
+package DSA.RaveeshConcepts;
+
+import java.util.Comparator;
+
+public class lengthEmpName implements Comparator{
+
+
+    @Override
+    public int compare(Object arg0, Object arg1) {
+       Emp e1 = (Emp)arg0;
+       Emp e2 = (Emp)arg1;
+
+       return e1.name.length() - e2.name.length();
+    }
+    
+}

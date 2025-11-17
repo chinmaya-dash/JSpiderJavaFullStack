3c6f875f3c6819a2c6d00ef7e1753bc250b5a057
Chinmaya Dash <chinmayadash442004@gmail.com>
2025-11-17 18:27:30 +0530

    updated

--- src/AdvancedJavaAndProjects/MavenDemo/src/main/java/Demo/demo.java
+++ src/AdvancedJavaAndProjects/MavenDemo/src/main/java/Demo/demo.java
@@ -23,56 +23,54 @@ public class demo {
 //            step 3 - Create prepared Statement Object
 //            String sql = "insert into student values(?, ?, ?)";
 //            String sql = "delete from student where id = ?";
-              String sql = "update student set age=? where name=?" ;
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-
-//            preparedStatement.setInt(1,13);
-            preparedStatement.setInt(1, 24);
-            preparedStatement.setString(2, "Gayana");
-
-            preparedStatement.addBatch();
-
-//            preparedStatement.setInt(1,14);
-            preparedStatement.setInt(1, 25);
-            preparedStatement.setString(2, "Smruti");
-
-
-            preparedStatement.addBatch();
-
-//            preparedStatement.setInt(1,15);
-            preparedStatement.setInt(1, 24);
-            preparedStatement.setString(2, "Udit");
-
-            preparedStatement.addBatch();
-
-
-
-            preparedStatement.executeBatch();
-
-
-
-
-//            BatchExecution
-
-
-
-//            BatchExecution
-
-
-
-
+//              String sql = "update student set age=? where name=?" ;
+//            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+//
+////            preparedStatement.setInt(1,13);
+//            preparedStatement.setInt(1, 24);
+//            preparedStatement.setString(2, "Gayana");
+//
+//            preparedStatement.addBatch();
+//
+////            preparedStatement.setInt(1,14);
+//            preparedStatement.setInt(1, 25);
+//            preparedStatement.setString(2, "Smruti");
+//
+//
+//            preparedStatement.addBatch();
+//
+////            preparedStatement.setInt(1,15);
+//            preparedStatement.setInt(1, 24);
+//            preparedStatement.setString(2, "Udit");
+//
+//            preparedStatement.addBatch();
+//
+//            preparedStatement.executeBatch();
 
 //            step 3 - creating statement
-//            Statement statement = connection.createStatement();
 //            String sql = "insert into student values(111, 'Chinmaya Dash', 24)";
 //            String sql2 = "delete from lectures where Name = 'deepak'";
 //            String sql2 = "update student set id = 100, name = 'chinu', age = 25 where name = 'Chinmaya Dash'";
 //                statement.execute(sql2);
 
+//            For select query
+            String sql = "select * from student where name = ?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, "chinu");
+//            preparedStatement.setString(1, "chinu");
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+                System.out.println("id : "+resultSet.getInt(1)+", name : "+resultSet.getString(2)+", age : "+resultSet.getInt(3));
+            }
+
+
 //            step 4 - executing query
 
+            System.out.println("All data extracted");
 //            boolean flag = statement.execute(sql2);
-            System.out.println("Data Saved...");
+//            System.out.println("Data Saved...");
 //            System.out.println(flag);
 
 //            step 5 - closing connection

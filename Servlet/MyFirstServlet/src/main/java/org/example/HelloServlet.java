9626fb8c9f4710138104e23773e3412f299bbea9
Chinmaya Dash <chinmayadash442004@gmail.com>
2025-12-03 17:19:33 +0530

    js Functions

--- Servlet/MyFirstServlet/src/main/java/org/example/HelloServlet.java
+++ Servlet/MyFirstServlet/src/main/java/org/example/HelloServlet.java
@@ -1,4 +1,22 @@
 package org.example;
 
-public class HelloServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Hello from Servlet!</h1>");
+        out.println("<p>Your Servlet is working correctly!</p>");
+    }
 }
---
+++ WebTechnology/JAVASCRIPT/basics/app.js
@@ -0,0 +1,5 @@
+ function calc(num1, num2){
+            console.log(num1+num2);
+        }
+        calc(5, "5");
+        console.log(5+"5");
